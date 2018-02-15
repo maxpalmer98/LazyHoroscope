bd54deab4599589531498d5b39b0a9641c64af15
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-02-14 17:42:58 -0800

    Evaluates first letter for fortune

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:Maxwell Palmer
+//Authors: Maxwell Palmer
 #include<iostream>
 #include<string>
 
@@ -13,13 +13,25 @@ int main()
   //get user input
   cout<<"What is your first name?"<<endl;
   cin>> fname;
-  cout<<"What is you last name"<<endl;
+  cout<<"What is you last name?"<<endl;
   cin>> lname;
   cout<<"Welcome "<<fname[0]<<"."<<lname[0]<<". here is your fortune..."<<endl;
   int lucky;
   lucky = (int)fname.length() + lname.length();
+  
   //tell fortune
   cout<<"your lucky number is "<<lucky<<endl;
+  if (fname[0] == 'E'||fname[0] == 'A'|| fname[0] == 'O'||fname[0] == 'I'||fname[0] == 'U'||fname[0] == 'a'||fname[0] == 'e'||fname[0] == 'i'||fname[0] == 'o'||fname[0] == 'u')
+  {
+  cout<<"you are destined to be famous "<<endl;
+  cout<<"you have already met your true love"<<endl;
+  cout<<"have a good day"<<endl;
+  }
+  else
+  {
+  cout<<"you should keep a low profile"<<endl;
+  cout<<"have a good day"<<endl;
+  }
 
   return 0;
 }
